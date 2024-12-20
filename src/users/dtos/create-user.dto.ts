449f5d22430fec5e95f6029da9f1669b03f33379
aryanlandge-01 <landgearyan0@gmail.com>
2024-12-20 12:10:49 +0530

    Built-In pipes and Intro to DT0 class validator

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,41 @@
+import { 
+    IsEmail, 
+    IsNotEmpty, 
+    IsOptional, 
+    IsString,  
+    Matches,  
+    MaxLength,  
+    MinLength } 
+from "class-validator";
+
+
+
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(96)
+    firstName: string;
+
+    @IsString()
+    @IsOptional()
+    @MinLength(3)
+    @MaxLength(96)
+    lastName?: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    @Matches(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+        {
+        message: 'Mininum 8 character,1 letter,1 number,1 special character.'
+        }
+    )
+    password: string
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,39 @@
-import { Controller,Get ,Post,Put,Patch,Delete} from '@nestjs/common';
+import { 
+    Controller,
+    Get,
+    Post,
+    Put,
+    Patch,
+    Delete, 
+    Param,
+    Query,
+    Body,
+    Req,
+    ParseIntPipe,
+    DefaultValuePipe
+} from '@nestjs/common';
+
+
 
 @Controller('users')
 export class UsersController {
-    @Get()
-    public getUsers(){
+    @Get('/:id/:name?')
+    public getUsers(
+        @Param('id',ParseIntPipe) id: number | undefined,
+        @Query('limit',new DefaultValuePipe(10),ParseIntPipe) limit: number | undefined,
+        @Query('page' ,new DefaultValuePipe(1),ParseIntPipe) page: number | undefined){
+        console.log("Types of Id: ",typeof id);
+        console.log(
+            "Types of query",
+            limit,
+            page
+        );
         return "You sent a get request to users endpoint."
     }
 
     @Post()
-    public postUser(){
+    public postUser(@Body() request: any){
+        console.log(request);
         return "User added successfully."
     }
 }
