3c408c9d7c8606b13f8db11c00f187a9354466ab
aryanlandge-01 <landgearyan0@gmail.com>
2024-12-26 17:31:57 +0530

    Add updatePost endpoint and PatchPostDto for post updates with Swagger documentation and Mapped types using swagger

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,12 +3,14 @@ import {
     Get,
     Param,
     Body,
-    Post
+    Post,
+    Patch
 } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/users/dtos/create-user.dto';
 import { CreatePostDto } from './dtos/create-post.dto';
+import { PatchPostDto } from './dtos/patch-post.dto';
 
 
 @Controller('posts')
@@ -24,10 +26,26 @@ export class PostsController {
     public getPosts(@Param('userId') userId: string){
         return this.postsService.findAll(userId);
     }
-
+    
+    @ApiOperation({ summary: 'Create a new post' })
+    @ApiResponse({
+        status: 201,
+        description: 'The record has been successfully created.'
+    })
     @Post('/create/:userId?')
     public createPost(@Body() createPostDto: CreatePostDto){
         console.log(createPostDto)
         return 'Post added';
+    }
+    
+    @ApiOperation({ summary: 'Update a post' })
+    @ApiResponse({
+        status: 200,
+        description: 'The record has been successfully updated.'
+    })
+    @Patch()
+    public updatePost(@Body() patchPostDto: PatchPostDto){
+        console.log(patchPostDto)
+        return 'Post updated';
     }
 }
